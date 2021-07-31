3625b666814f51e1fde3aed02d98041275ed7f0c
Gül Önal <50238339+Gulonal@users.noreply.github.com>
2021-07-31 12:32:08 +0300

    c# ile mongo db kütüphanesi kullanarak yapilmis proje

---
+++ ConsoleApp14/Ogrenci.cs
@@ -0,0 +1,51 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    public class Ogrenci
+    {
+        public ObjectId _id { get; set; } = ObjectId.GenerateNewId();
+        public string Isim { get; set; }
+        public string Soyad { get; set; }
+        public string No { get; set; }
+       
+
+
+        public IMongoDatabase GetDatabase()
+        {
+            return new MongoClient().GetDatabase("OKUL");
+        }
+        public bool Insert()
+        {
+            try
+            {
+                IMongoDatabase _database = GetDatabase();
+                var collection = _database.GetCollection<BsonDocument>("Sinif");
+                if (collection == null)
+                {
+                    _database.CreateCollection("Sinif");
+                    collection = _database.GetCollection<BsonDocument>("Sinif");
+                }
+                collection.InsertOne(this.ToBsonDocument());
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public DeleteResult Delete(string no)
+        {
+                IMongoDatabase _database = GetDatabase();
+                var collection = _database.GetCollection<BsonDocument>("Sinif");
+                var filter = Builders<BsonDocument>.Filter.Eq("No", no);
+                var result = collection.DeleteMany(filter);
+                return result;
+
+        }
+    }
+}
---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Kayıt Eklemek İçin E Kayıt Silmek için S Tuşuna Basınız");
+                string secim = Console.ReadLine();
+                if (secim.ToLower() == "e")
+                {
+                    Console.WriteLine("Kaydı Eklenecek Öğrencinin Adını Giriniz");
+                    string isim = Console.ReadLine();
+                    Console.WriteLine("Kaydı Eklenecek Öğrencinin Soyadını Giriniz");
+                    string soyad = Console.ReadLine();
+                    Console.WriteLine("Kaydı Eklenecek Öğrencinin Numarasını Giriniz");
+                    string no= Console.ReadLine();
+                    Ogrenci o=new Ogrenci();
+                    o.Isim = isim;
+                    o.Soyad = soyad;
+                    o.No = no;
+                    if (o.Insert())
+                    {
+                        Console.WriteLine("Bir Kayıt Başarıyla Eklendi");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Kayıt Ekleme Başarısız Lütfen Tekrar Deneyiniz.");
+                    }
+
+                }
+                else if (secim.ToLower() == "s")
+                {
+                    Console.WriteLine("Kaydı Silinecek Öğrencinin Numarasını Giriniz");
+                    string no= Console.ReadLine();
+                    Ogrenci o=new Ogrenci();
+                    DeleteResult sonuc=o.Delete(no);
+                    Console.WriteLine(sonuc.DeletedCount +" Kayıt Başarıyla Silindi");
+                }
+            }
+        }
+    }
+}
